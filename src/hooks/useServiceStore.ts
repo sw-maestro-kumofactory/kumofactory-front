1173e0dd4899d6126c91a94791dafe2f50c19ba1
HiGeuni <khyogeun_@naver.com>
2023-06-26 19:05:39 +0900

    Feat: Add immer for data immutability

--- src/hooks/useServiceStore.ts
+++ src/hooks/useServiceStore.ts
@@ -1,9 +1,11 @@
 import { devtools } from 'zustand/middleware';
 import create from 'zustand';
-import { IComponent, Services } from '@/src/types';
+import { IComponent, ServiceOptions, Services } from '@/src/types';
+import { immer } from 'zustand/middleware/immer';
+import { v1 } from 'uuid';
 
 interface ServiceState {
-  services: IComponent[];
+  services: Record<string, Services>;
   selectedService: IComponent | null;
   isMouseDown: boolean;
   interval: {
@@ -14,75 +16,93 @@ interface ServiceState {
 interface ServiceActions {
   action: {
     addService: (service: Services) => void;
-    removeService: (id: number) => void;
+    removeService: () => void;
     clearService: () => void;
     onClickGrid: (event: React.MouseEvent) => void;
     onMouseDownService: (event: React.MouseEvent, service: IComponent | null) => void;
     onMouseUpService: (event: React.MouseEvent) => void;
     onMouseMoveService: (event: React.MouseEvent) => void;
+    setOptions: (options: ServiceOptions) => void;
   };
 }
 
 const useServiceStore = create<ServiceState & ServiceActions>()(
-  devtools((set) => ({
-    services: [],
-    selectedService: null,
-    interval: {
-      x: 0,
-      y: 0,
-    },
-    isMouseDown: false,
-    action: {
-      onClickGrid: (event: React.MouseEvent) => {
-        set(() => ({ selectedService: null }));
+  devtools(
+    immer((set, get) => ({
+      services: {},
+      selectedService: null,
+      interval: {
+        x: 0,
+        y: 0,
       },
-      onMouseDownService: (event: React.MouseEvent, service: IComponent | null) => {
-        set((state) => {
-          if (service) {
-            const newInterval = {
-              x: event.clientX - service.x,
-              y: event.clientY - service.y,
-            };
+      isMouseDown: false,
+      action: {
+        onClickGrid: (event: React.MouseEvent) => {
+          set(() => ({ selectedService: null }));
+        },
+        onMouseDownService: (event: React.MouseEvent, service: IComponent | null) => {
+          set((state) => {
+            if (service) {
+              const newInterval = {
+                x: event.clientX - service.x,
+                y: event.clientY - service.y,
+              };
+              return {
+                isMouseDown: true,
+                selectedService: service ? service : null,
+                interval: newInterval,
+              };
+            }
             return {
-              isMouseDown: true,
-              selectedService: service ? service : null,
-              interval: newInterval,
+              ...state,
+              selectedService: null,
             };
-          }
-          return {
-            ...state,
-            selectedService: null,
-          };
-        });
-      },
-      onMouseUpService: (e: React.MouseEvent) => {
-        set(() => ({
-          isMouseDown: false,
-        }));
-      },
-      onMouseMoveService: (event: React.MouseEvent) => {
-        set((state) => {
-          if (state.selectedService && state.isMouseDown) {
-            const updatedServices = [...state.services];
-            const index = updatedServices.findIndex((item) => item.id === state.selectedService?.id);
-            updatedServices[index].x = event.clientX - state.interval.x;
-            updatedServices[index].y = event.clientY - state.interval.y;
-            return {
-              services: updatedServices,
+          });
+        },
+        onMouseUpService: (e: React.MouseEvent) => {
+          set(() => ({
+            isMouseDown: false,
+          }));
+        },
+        onMouseMoveService: (event: React.MouseEvent) => {
+          set((state) => {
+            if (state.selectedService && state.isMouseDown) {
+              const newX = Math.round((event.clientX - state.interval.x) / 18) * 18;
+              const newY = Math.round((event.clientY - state.interval.y) / 18) * 18;
+              state.services[state.selectedService.id].x = newX;
+              state.services[state.selectedService.id].y = newY;
+              return state;
+            }
+          });
+        },
+        addService: (service: Services) =>
+          set((state) => {
+            const id = v1().toString();
+            state.services[id] = {
+              ...service,
+              id,
             };
+          }),
+        removeService: () =>
+          set((state) => {
+            if (state.selectedService !== null) {
+              state.services = Object.fromEntries(
+                Object.entries(state.services).filter(([key, service]) => service.id !== state.selectedService!.id),
+              );
+            }
+            state.selectedService = null;
+          }),
+        clearService: () => set(() => ({ services: [], selectedService: null })),
+        setOptions: (options: ServiceOptions) => {
+          if (get().selectedService) {
+            set((state) => {
+              state.services[get().selectedService!.id].options = options;
+            });
           }
-          return state;
-        });
+        },
       },
-      addService: (service: IComponent) =>
-        set((state) => ({ services: [...state.services, service], selectedService: null })),
-      removeService: (id: number) =>
-        set((state) => ({
-          services: state.services.filter((service) => service.id !== id),
-        })),
-      clearService: () => set(() => ({ services: [], selectedService: null })),
-    },
-  })),
+    })),
+  ),
 );
 
 export const useServiceActions = () => useServiceStore((state) => state.action);
