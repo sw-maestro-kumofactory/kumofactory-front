16dbdede71d2e4129d17affcc2ef4c9fc733c781
HiGeuni <khyogeun_@naver.com>
2023-06-30 16:44:14 +0900

    Fix: Event Propagation problem

--- src/components/Blueprint/Area/AZ.tsx
+++ src/components/Blueprint/Area/AZ.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { IArea } from '@/src/types/Area';
 import useBlueprintStore from '@/src/hooks/Store/blueprint/useBlueprintStore';
+import { Resizable } from 're-resizable';
 
 interface IProps {
   Area: IArea;
@@ -10,9 +11,23 @@ interface IProps {
 const AZ = ({ Area, activate }: IProps) => {
   const onMouseDownArea = useBlueprintStore((state) => state.AreaAction.onMouseDownArea);
   return (
-    <svg onMouseDown={(e) => onMouseDownArea(e, Area)}>
-      <rect x={Area.sx} y={Area.sy} fill='#C9DFF985' width={Area.width} height={Area.height} />
-      {activate && <rect x={Area.sx} y={Area.sy} fill='#C9DFF985' width={Area.width} height={Area.height} />}
+    <svg
+      onClick={(e) => {
+        e.stopPropagation();
+      }}
+      onMouseDown={(e) => {
+        e.stopPropagation();
+        onMouseDownArea(e, Area);
+      }}
+    >
+      {activate && <line strokeWidth='2' fill='red' x1={Area.sx} y1={Area.sy} x2={Area.sx + Area.width} y2={Area.sy} />}
+      <rect
+        x={Area.sx}
+        y={Area.sy}
+        fill={activate ? '#C9DFF9cc' : '#C0DFF985'}
+        width={Area.width}
+        height={Area.height}
+      />
       <text x={Area.sx} y={Area.sy}>
         Availability zone
       </text>
