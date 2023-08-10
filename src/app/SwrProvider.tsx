a582b625f8a7628a4dc3039554b9f0f1042bbfe5
HiGeuni <khyogeun_@naver.com>
2023-08-10 22:13:15 +0900

    Chore: For Build

--- src/app/SwrProvider.tsx
+++
@@ -1,5 +0,0 @@
-'use client';
-import { SWRConfig } from 'swr';
-export const SwrProvider = ({ children }: { children: React.ReactNode }) => {
-  return <SWRConfig value={{ revalidateOnFocus: false }}>{children}</SWRConfig>;
-};
